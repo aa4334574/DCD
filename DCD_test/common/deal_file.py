a64c19484c15f39c8fdc9117bc43a64434804a42
aa4334574 <1009046839@qq.com>
2019-06-28 18:20:34 +0800

    Add files via upload

---
+++ DCD_test/common/deal_file.py
@@ -0,0 +1,60 @@
+import os
+import requests
+import xlrd
+import logging
+from logging import handlers
+import time
+class Deal_file():
+    def __init__(self,path):
+        self.path = path
+
+    def write_excel(self):
+
+        readbook = xlrd.open_workbook(self.path)
+        sheet = readbook.sheet_by_index(0)
+        nrows = sheet.nrows
+        dic = {}
+        for i in range(nrows):
+            key = sheet.cell(i,0).value#获取第1列行第i行的表格值
+            value = sheet.cell(i,1).value#获取第2列第i行的表格值
+            dic['key'] = value
+        return dic
+
+    def write_log(self,object):
+        level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+        }#日志级别关系映射
+
+    def __init__(self,filename,level='info',when='D',backCount=3,fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        format_str = logging.Formatter(fmt)#设置日志格式
+        self.logger.setLevel(self.level_relations.get(level))#设置日志级别
+        sh = logging.StreamHandler()#往屏幕上输出
+        sh.setFormatter(format_str) #设置屏幕上显示的格式
+        th = handlers.TimedRotatingFileHandler(filename=filename,when=when,backupCount=backCount,encoding='utf-8')#往文件里写入#指定间隔时间自动生成文件的处理器
+        #实例化TimedRotatingFileHandler
+        #interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+        # S 秒
+        # M 分
+        # H 小时、
+        # D 天、
+        # W 每星期（interval==0时代表星期一）
+        # midnight 每天凌晨
+        th.setFormatter(format_str)#设置文件里写入的格式
+        self.logger.addHandler(sh) #把对象加到logger里
+        self.logger.addHandler(th)
+if __name__ == '__main__':
+    log = Logger('all.log',level='debug')
+    log.logger.debug('debug')
+    log.logger.info('info')
+    log.logger.warning('警告')
+    log.logger.error('报错')
+    log.logger.critical('严重')
+    Logger('error.log', level='error').logger.error('error')
+
+
+print (type(Deal_file(r'D:\可知新资源\1223\22.xlsx').write_excel()))
