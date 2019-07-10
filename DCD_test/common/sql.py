9a14a1909fd607296aa43558496d8b2e8ac7d6ab
aa4334574 <1009046839@qq.com>
2019-07-10 17:53:05 +0800

    修改mysql

--- DCD_test/common/sql.py
+++ DCD_test/common/sql.py
@@ -1,4 +1,5 @@
 import pymysql.cursors
+import time
 from DCD_test.common.deal_file import log
 #连接数据库
 # connect = pymysql.connect(
@@ -19,6 +20,11 @@ class mysql():
         password = 'db(*)2016',
         db = 'aqr_develop_v3')
         self.cursor = self.connect.cursor()
+    # def time_change(self,start_time,end_time):
+    #     start_time1 = time.strptime(start_time,"%Y-%m-%d %H:%M:%S")
+    #     end_time1 = time.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    #     print(start_time1-end_time1)
+
 
     def update(self,sql):
         success = True
@@ -33,9 +39,12 @@ class mysql():
 
     def select(self,sql):
         try:
+            start_time = time.time()
+            print('开始时间%s'%start_time)
             execute_mum = self.cursor.execute(sql)
-            print(execute_mum)
-
+            end_time = time.time()
+            print('结束时间%s'%end_time)
+            print(execute_mum,time.localtime(end_time-start_time))
         except Exception as e:
             log().logger.exception('e')
         return self.cursor.fetchall()
@@ -43,12 +52,19 @@ class mysql():
 
     def close(self):
         self.connect.close()
-sql = 'select * from datafile limit 1000'
-# print(mysql().select(sql))
-message = mysql().select(sql)
 
+
+sql = 'select * from datafile limit 1000'
+message = mysql().select(sql)
 for i in message:
-    print (i)
+    print(i[3])
+# for i in message:
+#     tem = 0
+#     for j in i:
+#         tem += 1
+#         if tem == 3:
+#             print(j)
+#
 
 
 # #
